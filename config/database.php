c82d5185a8dbf68999b8a8991a100757c7514a29
christian8mugisha <chancenshimiyimana8@gmail.com>
2026-07-01 10:09:06 +0200

    Implement PDO database connection

---
+++ config/database.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/constants.php';
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4",
+        DB_USER,
+        DB_PASS
+    );
+
+    /*
+    |--------------------------------------------------------------------------
+    | PDO Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+} catch (PDOException $e) {
+
+    die("Database Connection Failed: " . $e->getMessage());
+
+}
+?>
