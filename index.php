cbb8ff35985deb07513925cf8dc7dfbc0100c13e
christian8mugisha <chancenshimiyimana8@gmail.com>
2026-07-03 13:38:39 +0200

    Deployement successfuly comleted

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+
+    header("Location: dashboard/");
+    exit;
+}
+
+header("Location: auth/login.php");
+exit;
